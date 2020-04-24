ba9ad817feb540adde33d766230a948f9858c1c8
gnegovetic <gnegovetic@hotmail.com>
2020-04-24 16:22:27 -0700

    Update HelloWorld.cpp

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main()
 {
-    auto msg = "Hello from master with yet another change";
+    auto msg = "Hello from master with yet more changes";
     cout << msg << endl;
 
 	return 0;
