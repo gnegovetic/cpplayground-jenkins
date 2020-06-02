842c321c789fa9b9028270468defef133b1f50d5
Goran Negovetic <Goran.Negovetic@tektronix.com>
2020-06-02 09:08:07 -0700

    Added param input.

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -1,10 +1,15 @@
 #include <iostream>
 using namespace std;
 
-int main()
+int main(int argc, const char* argv[])
 {
     auto msg = "Hello from master ---";
     cout << msg << endl;
+
+    if (argc > 1) 
+    {
+        cout << " --- With param." << endl;
+    }
 
 	return 0;
 }
